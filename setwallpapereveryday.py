d5c2e089808e8bc2525cdc35d66fb96a36f46c05
shinkeika <shinkeika>
2019-11-01 10:46:53 +0800

    添加header和cookie

--- setwallpapereveryday.py
+++ setwallpapereveryday.py
@@ -8,20 +8,47 @@ import subprocess
 import re
 from bs4 import BeautifulSoup
 from PIL import Image, ImageFont, ImageDraw
+import random
 
+user_agent = [
+	"Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+	"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+	"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+	"Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+	"Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+	"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+	"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52"
+]
 
+HEADER = {
+'User-Agent': random.choice(user_agent),  # 浏览器头部
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3', # 客户端能够接收的内容类型
+'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8', # 浏览器可接受的语言
+'Connection': 'keep-alive', # 表示是否需要持久连接
+'cookie':'MUID=225D735E7B5E63B7034A7D5F7F5E6026; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=DD61EE30B05A4FC38EE40AFC2CD365CB&dmnchg=1; MUIDB=225D735E7B5E63B7034A7D5F7F5E6026; _UR=MC=1; _EDGE_S=mkt=zh-cn&SID=1DA4BE4611D16EE83F1AB04010196F53; _FP=hta=on; ipv6=hit=1572577825564&t=4; SRCHUSR=DOB=20191030&T=1572574252000; ENSEARCH=BENVER=1; _SS=SID=1DA4BE4611D16EE83F1AB04010196F53&HV=1572575901&bIm=003793; SRCHHPGUSR=CW=865&CH=702&DPR=2&UTC=480&WTS=63708171024; ULC=P=1AE27|13:2&H=1AE27|44:3&T=1AE27|41:3:74'
+}
 
 def get_wallpaper():
-	URL = "https://www.bing.com"
+	URL = "https://www.bing.com/"
 
-	html = requests.get(URL).text
+	html = requests.get(URL,headers=HEADER).text
 	soup = BeautifulSoup(html, 'lxml')
 	img_ul = soup.find_all('link', {"id": "bgLink"})
-
+	filename = ''
 	# save file
 	for ul in img_ul:
 	    r = requests.get(URL + ul['href'], stream=True)
-	    filename = '/Users/shinkeika/Pictures/net.neolib.BingDailyWallpaper/' + time.strftime('%Y_%m_%d',time.localtime(time.time())) + '.jpeg'
+	    filename = '/Users/shinkeika/Pictures/net.neolib.BingDailyWallpaper/' + time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime(time.time())) + '.jpeg'
 	    with open(filename, 'wb+') as f:
 	        for chunk in r.iter_content(chunk_size=128):
 	            f.write(chunk)
